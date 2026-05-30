e2ec4d14b1711cd63e5ba82b1cec0e633e1fb4de
Lydex90 <ozzwizard_91@hotmail.com>
2026-05-30 16:54:48 +0200

    Multiple page generation now possible

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-from textnode import TextType, TextNode
 import os
 import shutil
 import re
-from markdwn_to_text import markdown_to_html_node
+from page_generate import copy_files, extract_title, generate_page, generate_pages_recursive
 
 
 def main():
@@ -11,38 +10,7 @@ def main():
     shutil.rmtree(public_path)
     os.makedirs(public_path)
     copy_files(static_path, public_path)
-    generate_page("content/index.md","template.html","public/index.html")
-
-def copy_files(src, dst):
-    for item in os.scandir(src):
-        if item.is_dir():
-            new_dst = os.path.join(dst, item.name)
-            os.makedirs(new_dst, exist_ok=True)
-            copy_files(item.path, new_dst)
-        else:
-            shutil.copy(item.path, dst)
-
-def extract_title(markdown):
-    h1 = re.search(r'^# (.+)$', markdown, re.MULTILINE)
-    if not h1:
-        raise Exception("No header found")
-    return h1.group(1)
-
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
-    with open(from_path, 'r') as f:
-        from_content = f.read()
-    with open(template_path, 'r') as f:
-        template_content = f.read()
-    from_converted = markdown_to_html_node(from_content)
-    from_converted = from_converted.to_html()
-    title = extract_title(from_content)
-    result = template_content.replace("{{ Title }}", title).replace("{{ Content }}", from_converted)
-    
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path, 'w') as f:
-        f.write(result)
-     
+    generate_pages_recursive("content","template.html","public")
 
 
 main()
---
+++ src/page_generate.py
@@ -0,0 +1,50 @@
+from markdwn_to_text import markdown_to_html_node
+from textnode import TextType, TextNode
+import os
+import shutil
+import re
+
+
+def copy_files(src, dst):
+    for item in os.scandir(src):
+        if item.is_dir():
+            new_dst = os.path.join(dst, item.name)
+            os.makedirs(new_dst, exist_ok=True)
+            copy_files(item.path, new_dst)
+        else:
+            shutil.copy(item.path, dst)
+
+def extract_title(markdown):
+    h1 = re.search(r'^# (.+)$', markdown, re.MULTILINE)
+    if not h1:
+        raise Exception("No header found")
+    return h1.group(1)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
+    with open(from_path, 'r') as f:
+        from_content = f.read()
+    with open(template_path, 'r') as f:
+        template_content = f.read()
+    from_converted = markdown_to_html_node(from_content)
+    from_converted = from_converted.to_html()
+    title = extract_title(from_content)
+    result = template_content.replace("{{ Title }}", title).replace("{{ Content }}", from_converted)
+    
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, 'w') as f:
+        f.write(result)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    get_all_files = os.listdir(dir_path_content)
+    print(f"generating page recursive on these files {get_all_files}")
+    for file in get_all_files:
+        full_path = os.path.join(dir_path_content, file)
+        print(f"checking file {file}")
+        if full_path.endswith('.md'):
+            dest_path = os.path.join(dest_dir_path, file.replace(".md", ".html"))
+            generate_page(full_path,template_path,dest_path)
+        elif os.path.isdir(full_path):
+            new_dest = os.path.join(dest_dir_path, file)
+            generate_pages_recursive(full_path, template_path, new_dest)
+        
