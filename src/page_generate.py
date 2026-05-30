e24807b7a940a7e028643af40a8ef666164d1793
Lydex90 <ozzwizard_91@hotmail.com>
2026-05-30 17:33:03 +0200

    fixed the replace bug, fixing src and href links

--- src/page_generate.py
+++ src/page_generate.py
@@ -22,6 +22,7 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
+    print(f"base_path: {base_path}")
     with open(from_path, 'r') as f:
         from_content = f.read()
     with open(template_path, 'r') as f:
